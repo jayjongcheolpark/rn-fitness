1103436da4981f552b6cf55dd9e478d4746828c1
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-14 23:16:51 -0500

    Setup eslint

--- App.test.js
+++ App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from './App'
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
+  const rendered = renderer.create(<App />).toJSON()
+  expect(rendered).toBeTruthy()
+})
